a8f757b7aa40d1c109d8dce6b53b775116f23d95
Debbie Cheng <dqrcheng@gmail.com>
2021-10-11 00:02:32 +1100

    Added readme. Draft main.ts

---
+++ README.md
@@ -0,0 +1,30 @@
+**Toy Robot**
+Task details are from [ZONE](https://zone.github.io/frontend/toy-robot).
+I have decided to use **vanilla JS/TS**.
+
+**10/10/2021: Task is still WIP.**
+
+**What**
+* A toy robot on a table top
+* A grid of 5 x 5 units, there are no obstructions.
+* Commands that you can issue to your robot, allowing it to roam around the table top.
+
+**Task**
+* Create an app that allows commands to be issued to the robot.
+* The robot should be prevented from failing off the table top to its destruction.
+* A failed command should not stop the app, valid commands should be allowed.
+* The application should discard all commands until a valid place() command has been executed.
+
+**Inputs**
+* `place(x, y, facing)`
+    * x and y are integers that relate to a location on the grid. Values that are outside the boundary of the grid should not be allowed.
+    * facing is a string referencing the direction the robot is facing. Values NORTH, SOUTH, EAST or WEST are allowed.
+* `move()`
+    * Moves the robot 1 grid unit in the direction it is facing unless that movement will cause the robot to fall off the grid.
+* `left()`
+    * Rotate the robot 90° counterclockwise.
+* `right()`
+    * Rotate the robot 90° clockwise.
+* `report()`
+    * Outputs the robot's current grid location and facing direction.
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { bulkAppendById, createGrid } from './utils/helper';
-import { mainId, commandFormId, commandInputId } from './utils/constants';
+import {
+  commandErrorId,
+  commandFormId,
+  commandInputId,
+  mainId,
+} from './utils/constants';
 import { formLayer, mainLayer } from './elements/layers';
-import { formatPlaceCommands } from './utils/helper';
+import { formatPlaceCommands, formatUserInput } from './utils/helper';
 import { place } from './utils/commands';
+import { initialValidate } from './utils/validation';
 
 bulkAppendById(mainId, mainLayer);
 bulkAppendById(commandFormId, formLayer);
@@ -10,13 +16,32 @@ createGrid();
 
 const commandForm = document.getElementById(commandFormId);
 const commandInput = document.getElementById(commandInputId) as HTMLInputElement;
+const commandErrorLog = document.getElementById(commandErrorId);
+
+const handleBlur = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+
+  if (target) {
+    const errorLog = initialValidate(target);
+    if (errorLog && commandErrorLog) {
+      commandErrorLog.textContent = errorLog
+    } else if (commandErrorLog) {
+      commandErrorLog.textContent = '';
+    }
+  }
+}
 
 const handleSubmit = (e: Event) => {
   e.preventDefault();
+  if (commandErrorLog && commandErrorLog.textContent) return;
+
   if (commandInput && commandInput?.value) {
-    const position = formatPlaceCommands(commandInput.value);
+    const userInput = formatUserInput(commandInput.value);
+
+    const position = formatPlaceCommands(userInput);
     position ? place(position) : null;
   };
 };
 
 commandForm?.addEventListener('submit', handleSubmit);
+commandInput?.addEventListener('blur', handleBlur);
